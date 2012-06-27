dfb556d6e33350aa7d72965c50d261af3e268740
Adam Spence <aspence1@gmail.com>
2012-06-27 15:46:51 +0100

    fixing bug in optional port

--- rocket_engine/management/commands/runserver.py
+++ rocket_engine/management/commands/runserver.py
@@ -6,6 +6,8 @@ from ... import PROJECT_DIR, DEFAULT_PORT
 class Command(BaseCommand):
 
     def run_from_argv(self, argv):
-        port = [arg if arg else '--port=%s' % DEFAULT_PORT for arg in argv if 'port' in arg]
+        port = ['--port=%s' % DEFAULT_PORT]
+        if argv:
+            port = [arg for arg in argv if 'port' in arg]
         dev_appserver_main.PrintUsageExit = lambda x: ""
         dev_appserver_main.main(['runserver', PROJECT_DIR] + argv[2:] + port)
